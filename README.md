710a802798c031311ec13be0e985d24c19f4901c
BBNganNguyen <ntnngan1@gmail.com>
2022-12-11 22:39:37 -0500

    Revere A String

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DataStructuresAndAlgorithm
+- This is personal repo for me to understand more about Data Structure and Algorithm. 
+- Questions and solutions I get from myself, internet (multiple resources like leetcode, undemy ..).
+1. Reverse a String [ReversingAString](https://github.com/BBNganNguyen/DataStructuresAndAlgorithm/blob/master/src/main/java/array/ReversingAString.java):  
+  The problem is that we want to reverse a T[] array in O(N) linear time complexity and we want the algorithm to be in-place as well - so the algorithm can not use additional memory!
+  For example: input is [1,2,3,4,5] then the output is [5,4,3,2,1]
---
+++ src/main/java/com/datastructures/array/ReverseAString.java
@@ -0,0 +1,27 @@
+package com.datastructures.array;
+
+public class ReverseAString {
+    /**
+     * Solution 1:
+     * Loop for the half of string and swap the position of first index and last index
+     * @param inputStr
+     * @return reverseString of an inputString
+     */
+    public static String reverseString(String inputStr) {
+        if (inputStr.isEmpty()) {
+            return "";
+        }
+        char[] str = inputStr.toCharArray();
+        for (int i = 0; i < str.length / 2; i++) {
+            swap(str, i, str.length - 1);
+        }
+        return String.valueOf(str);
+    }
+
+    private static void swap(char[] str, int i, int j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+}
